88b1130447ba75799a49ea8b76156d8607f703b1
Tony Bowden <tony@tmtm.com>
2021-12-12 11:54:30 +0200

    initial scraper

--- bin/scraper/official.rb
+++ bin/scraper/official.rb
@@ -7,17 +7,20 @@ require 'pry'
 class MemberList
   class Member
     def name
-      noko.css('.name').text.tidy
+      Name.new(
+        full:     noko.css('h4').first.text.gsub('&nbsp', ' ').tidy,
+        prefixes: %w[Prime Minister H.E.Dr H.E.Mr H.E.Eng. H.E HE .H.E.Dr H.E.Prof Eng. Mr.]
+      ).short
     end
 
     def position
-      noko.css('.position').text.tidy
+      noko.css('h3').first.text.tidy
     end
   end
 
   class Members
     def member_container
-      noko.css('.member')
+      noko.css('.ministercont')
     end
   end
 end
